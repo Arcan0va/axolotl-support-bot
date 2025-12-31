49d4113bf6d639c60412e4f4c995862996f1046c
Arcan0va <pickels.king.off@gmail.com>
2025-12-31 14:34:00 +0100

    Update register.js

--- functions/register.js
+++ functions/register.js
@@ -1,20 +1,30 @@
-// Remplace par les infos de ton bot
-const DISCORD_APP_ID = "<1455877157581754453>"
-const DISCORD_BOT_TOKEN = "<MTQ1NTg3NzE1NzU4MTc1NDQ1Mw.GD6ThR.dV2zwvf6Ife8tp6zYfWjzM7eTupAyFdCBOTuRw>"
+export default {
+  async fetch(request, env) {
+    const DISCORD_APP_ID = env.DISCORD_APP_ID;
+    const DISCORD_BOT_TOKEN = env.DISCORD_BOT_TOKEN;
 
-fetch(`https://discord.com/api/v10/applications/${DISCORD_APP_ID}/commands`, {
-  method: "POST",
-  headers: {
-    "Authorization": `Bot ${DISCORD_BOT_TOKEN}`,
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    name: "support",
-    description: "Ouvre le menu de support Axolotl",
-    type: 1, // Slash command
-    dm_permission: true
-  })
-})
-.then(res => res.json())
-.then(console.log)
-.catch(console.error)
+    try {
+      const response = await fetch(`https://discord.com/api/v10/applications/${DISCORD_APP_ID}/commands`, {
+        method: "POST",
+        headers: {
+          "Authorization": `Bot ${DISCORD_BOT_TOKEN}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: "support",
+          description: "Ouvre le menu de support Axolotl",
+          type: 1,
+          dm_permission: true
+        })
+      });
+
+      const data = await response.json();
+      return new Response("Commande /support enregistrée !\n" + JSON.stringify(data), {
+        headers: { "Content-Type": "application/json" }
+      });
+
+    } catch (err) {
+      return new Response("Erreur : " + err.message, { status: 500 });
+    }
+  }
+}
